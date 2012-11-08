acb926426d8498f0d5343cf9ba849dbd010c4d16
Yilei Wang <yileiwang84@gmail.com>
2012-11-08 15:53:27 -0500

    delete file2.txt and add ast.ml

---
+++ compiler/ast.ml
@@ -0,0 +1,26 @@
+type op = 	Plus | Minus | Times | Divide | Mod | And | Or | Lt | Gt | Eq | Leq | Geq | Neq | Mask
+type expr = (* Expressions *)
+Literal of int (* 42 *)
+| Id of string (* foo *)
+| Binop of expr * op * expr (* a + b *)
+| Assign of string * expr (* foo <- 42 *)
+| Output of string * expr (* canvas -> out *)
+| Call of string * expr list (* foo(1, 25) *)
+| Noexpr (* for (;;) *)
+
+type stmt = (* Statements *)
+Block of stmt list (* { ... } *)
+| Expr of expr (* foo <- bar + 3; *)
+| Return of expr (* return 42; *)
+| If of expr * stmt (* if (foo = 42) {} *)
+| If_else of expr * stmt * stmt (* if (foo = 42) {} else {} *)
+| For of expr * expr * expr * stmt (* for i<-0|i<10|i<-i+1 { ... } *)
+
+type func_decl = {
+fname : string; (* Name of the function *)
+args : string list; (* Formal argument names *)
+locals : string list; (* Locally defined variables *)
+body : stmt list;
+}
+
+type program = stmt list * func_decl list (* global vars, funcs *)
