c82221c1634740d1723dee3fcaffc6ac645c0808
Joe Lee <joeyoung.lee@gmail.com>
2012-12-13 16:40:58 -0500

    starting work on sast.

--- compiler/sast.ml
+++ compiler/sast.ml
@@ -1,12 +1,27 @@
-(* FILENAME :  ezatypes.ml
- * AUTHOR(s):  Joe Lee (jyl2157), Dmitriy Gromov (dg2720)
- * PURPOSE  :  Define types for semantic analysis.
+(* FILENAME :  sast.ml
+ * AUTHOR(s):  Joe Lee (jyl2157)
+ * PURPOSE  :  Converts ast to sast.  
  *)
 
-type t = 
-    Void 
-  | Int 
+open Ast
+
+type exp_det = 
+    IntLiteral of int
+  | StrLiteral of string
+  | BoolLiteral of bool
+  | 
+
+type ezatypes = 
+    Int 
   | Bool
   | Char 
   | String 
   | Canvas
+
+(* takes Ast program and runs static semantic analysis (type errors, etc..) *)
+let semantic_checker program = 
+
+  (* eval returns a two-tuple consisting of the converted Sast element, and
+   * the Sast type *)
+  let eval expr env = function
+      Ast.IntLiteral(i) -> Sast.IntLiteral
